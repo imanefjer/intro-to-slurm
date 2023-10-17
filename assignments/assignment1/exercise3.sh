f4a984dff15786022700f412533ca3d9c8730306
imaneFjer <93489936+imanefjer@users.noreply.github.com>
2023-10-17 18:33:20 +0100

    Update exercise3.sh

--- assignments/assignment1/exercise3.sh
+++ assignments/assignment1/exercise3.sh
@@ -2,8 +2,6 @@
 #SBATCH --job-name=exercise3
 #SBATCH --output=exercise3.txt
 #SBATCH --array=1-10
-#SBATCH --cpus-per-task=44
-#SBATCH --mem=360G
 tail -n +2 config.txt | while IFS=' ' read -r task_id sample_name sex; do
     echo "This is array task $task_id, the sample name is $sample_name, and the sex is $sex" >> "exercise3.txt"
 done
